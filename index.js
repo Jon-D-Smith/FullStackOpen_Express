309e7ac09d0c93b333aec3aa37523cc0c479ff84
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-05 12:47:02 -0500

    Added check for existing person per 3.6

--- index.js
+++ index.js
@@ -25,13 +25,19 @@ let persons = [
     },
 ]
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
+// Get All Persons
+
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
+
+// Get individual person
 
 app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
@@ -44,10 +50,46 @@ app.get('/api/persons/:id', (req, res) => {
     
 })
 
+// Delete Person
+
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
     res.status(204).end()
+})
+
+// Generate ID and add person
+
+const generateId = () => {
+    const maxId = persons.length > 0 ? Math.max(...persons.map(person => person.id)) : 0
+
+    return maxId + 1
+}
+
+app.post('/api/persons', (req, res) => {
+    const {name, number} = req.body
+
+    if(!name || !number) {
+        return res.status(400).json({
+            error: "Content missing"
+        })
+
+    }
+
+    if(persons.find(person => person.name === name)){
+        return res.status(400).json({
+            error : "Name must be unique"
+        })
+    }
+
+    const person = {
+        name,
+        number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
 })
 
 app.get('/info', (req, res) => {
